073f23259ecafb41cedc950a50ef7e8383c71c82
Fmvmedeiros <84106516+Fmvmedeiros@users.noreply.github.com>
2024-10-30 13:17:00 +0000

    Simplificação. Tirei a segunda velocidade

--- main.ts
+++ main.ts
@@ -1,24 +1,12 @@
 input.onButtonPressed(Button.A, function () {
-    if (FrenteFast) {
-        radio.sendValue("andar", 11)
-    } else {
-        radio.sendValue("andar", 1)
-    }
-    FrenteFast = !(FrenteFast)
+    radio.sendValue("andar", 1)
 })
 input.onButtonPressed(Button.AB, function () {
     radio.sendValue("andar", 0)
 })
 input.onButtonPressed(Button.B, function () {
-    if (TrasFast) {
-        radio.sendValue("andar", 22)
-    } else {
-        radio.sendValue("andar", 2)
-    }
-    TrasFast = !(TrasFast)
+    radio.sendValue("andar", 2)
 })
-let TrasFast = false
-let FrenteFast = false
 basic.showLeds(`
     # # # # #
     . . . . #
@@ -27,8 +15,6 @@ basic.showLeds(`
     # # # # #
     `)
 radio.setGroup(2)
-FrenteFast = false
-TrasFast = false
 basic.forever(function () {
     radio.sendValue("virar", input.rotation(Rotation.Roll))
 })
